4de91adfa1a0615b832dca2f66767668453af358
valdasvabolis <thisisvaldasvabolis@gmail.com>
2017-10-15 00:37:03 +0300

    WIP persistent storage for votes

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hello</title>
+    <title>PHP-Polls</title>
   </head>
   <body>
     <h2>This is a poll</h2>
     <form action="/index.php" method="post">
-      <input type="radio" name="option" value="a"> A <br>
-      <input type="radio" name="option" value="b"> B <br>
-      <input type="radio" name="option" value="c"> C <br>
+      <input type="radio" name="option" value="A"> A <br>
+      <input type="radio" name="option" value="B"> B <br>
+      <input type="radio" name="option" value="C"> C <br>
+      <br>
       <button type="submit">Submit</button>
     </form>
+    <br>
 
     <?php
-      if(isset($_POST) && isset($_POST["option"])) {
+     $option = "";
+     if(isset($_POST) && isset($_POST["option"])) {
         $option = $_POST["option"];
-        echo "the last submitted option was " . $option;
       }
+    
+      $data = json_decode(file_get_contents("votes.json"));
+      $data[$option]++;
+      $json = json_encode($data);
+
+      file_put_contents("votes.json", $json);
+
     ?>
   </body>
 </html>
